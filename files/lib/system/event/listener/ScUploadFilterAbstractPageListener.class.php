f1070271dc199e5930e0e09bbed8a07e12402d31
Sascha Greuel <sascha@softcreatr.de>
2019-03-02 14:37:33 +0100

    Preparing release 1.1.0
    - Fixed a problem in a template listener
    - Added support for cover photos in user profiles

--- files/lib/system/event/listener/ScUploadFilterAbstractPageListener.class.php
+++ files/lib/system/event/listener/ScUploadFilterAbstractPageListener.class.php
@@ -5,30 +5,28 @@ use DateTime;
 
 /**
  * Provides an indicator wether to remove/replace
- * user avatars, or not.
+ * user avatars, profile banners, etc. or not.
  * 
  * @copyright	SoftCreatR Media
  * @license	GNU General Public License
  * @package	WoltLabSuite\Core\System\Event\Listener
  */
-class ScUploadFilterAvatarRemovalListener implements IParameterizedEventListener {
+class ScUploadFilterAbstractPageListener implements IParameterizedEventListener {
 	/**
 	 * @inheritDoc
 	 */
-	public function execute($eventObj, $className, $eventName, array &$parameters) {
-		if (!SC_UPLOAD_FREE_SUNDAY_HIDE_AVATARS) return;
-		
+	public function execute($eventObj, $className, $eventName, array &$parameters) {		
 		$tz = WCF::getUser()->getTimeZone();
 		$now = new DateTime('now', $tz);
 		$start = DateTime::createFromFormat('!Y-m-d', SC_UPLOAD_FREE_SUNDAY_START_DATE, $tz);
 		$end = DateTime::createFromFormat('!Y-m-d', SC_UPLOAD_FREE_SUNDAY_END_DATE, $tz);
 		
-		WCF::getTPL()->assign('uploadFilterRemoveAvatar', false);
+		WCF::getTPL()->assign('uploadFilterActive', false);
 		
 		if ($now->getTimestamp() < $start->getTimestamp() || $now->getTimestamp() > $end->getTimestamp()) {
 			return;
 		}
 		
-		WCF::getTPL()->assign('uploadFilterRemoveAvatar', true);
+		WCF::getTPL()->assign('uploadFilterActive', true);
 	}
 }
